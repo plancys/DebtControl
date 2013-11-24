e7994ba9b4f07113c699deff2a145998ef31f40d
kamilkalandyk <kamilkalandyk@cohesiva.com>
2013-11-24 15:11:17 +0100

    add sample navigation drawer app

--- debtcontrol-android/src/main/java/com/kalandyk/activities/HelloAndroidActivity.java
+++ debtcontrol-android/src/main/java/com/kalandyk/activities/HelloAndroidActivity.java
@@ -1,11 +1,12 @@
-package com.kalandyk;
+package com.kalandyk.activities;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+
+import com.kalandyk.R;
 
 public class HelloAndroidActivity extends Activity {
 
